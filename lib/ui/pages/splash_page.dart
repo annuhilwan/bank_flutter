0cdf60717e197c305e0d4fca6ebc347f6e405e32
annuhliwan <macpro@MacBook-Pro.local>
2024-02-09 06:07:49 +0700

    first commit

---
+++ lib/ui/pages/splash_page.dart
@@ -0,0 +1,51 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_bank/shared/theme.dart';
+import 'package:flutter_bank/ui/pages/onboarding_page.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage ({Key?key}) : super(key: key);
+ 
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage>{
+
+  @override
+  void initState() {
+    //todo: implement initstate
+    super.initState();
+
+    Timer(const Duration(seconds: 2),(){
+      Navigator.push(
+        context, 
+        MaterialPageRoute(
+          builder: (context)=> const OnboardingPage(),
+            ),
+          );
+      });
+  }
+
+
+@override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: darkBackgroundColor,
+      body: Center(
+        child: Container(
+          width: 155,
+          height: 50,
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage(
+                'assets/img_logo_dark.png'
+              )
+            )
+            ),
+        ),
+      ),
+    );
+  }
+}
